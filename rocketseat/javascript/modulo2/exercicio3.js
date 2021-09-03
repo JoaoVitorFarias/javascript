d5f53e34dc56fc8865fafba832d435a10287f9fa
JoaoVitorFarias <jvlopesfarias@gmail.com>
2021-09-03 14:45:55 -0300

    Update repository

--- rocketseat/javascript/modulo2/exercicio3.js
+++
@@ -1,9 +0,0 @@
-var nomes = ["Diego", "Gabriel", "Lucas"];
-var list =document.querySelector('ul.listaNomes');
-
-for (nome of nomes){
-  var li = document.createElement('li');
-  var text = document.createTextNode(nome);
-  li.appendChild(text);
-  list.appendChild(li);
-}
