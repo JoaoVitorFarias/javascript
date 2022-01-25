a500dd39992792544cb46d6b24cabba6c88f6a39
JoaoVitorFarias <jvlopesfarias@gmail.com>
2022-01-25 09:40:21 -0300

    Start OO review

---
+++ OO/bytebank/index.js
@@ -0,0 +1,15 @@
+class Customer {
+    name;
+    cpf;
+    agency;
+    money;
+}
+
+const customer1 = new Customer();
+
+customer1.name = "John";
+customer1.cpf = "123456789";
+customer1.agency = 1001;
+customer1.money = 1325,20;
+
+console.log (customer1);
